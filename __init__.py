2f0cd1f870d937e4dec10e7ec14b83ae412059a8
rbitr <andrewmarble@gmail.com>
2021-05-01 13:43:54 -0400

    change init.py

--- __init__.py
+++
@@ -1,3 +0,0 @@
-
-import pytkml.core.processors as proc
-from pytkml.core.tkutils import ModelTester, test_args_dict
---
+++ pytkml/__init__.py
@@ -0,0 +1,2 @@
+import pytkml.core.processors as proc
+from pytkml.core.tkutils import ModelTester, test_args_dict
