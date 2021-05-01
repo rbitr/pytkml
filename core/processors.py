a915a75e4088ed5191fb7b19a295f5fafb40c325
rbitr <andrewmarble@gmail.com>
2021-05-01 12:55:45 -0400

    multiple influential examples

--- core/processors.py
+++ core/processors.py
@@ -17,6 +17,8 @@ from loguru import logger
 from pathlib import Path
 import json
 
+from typing import Iterable
+
 from pytkml.misc.logging import arrayToBase64IM
 
 LABELS = lambda x: x[1]
@@ -26,6 +28,9 @@ IDENTITY = lambda x: x
 # Reducers for converting output probabilities to a single value
 def first_element_reducer(x):
     return torch.Tensor([x[0] for x in x])
+
+def first_element_reducerInt(x):
+    return torch.IntTensor([x[0] for x in x])
 
 def argmax_reducer(x):
     return torch.argmax(x,dim=1)
@@ -90,17 +95,22 @@ def influence_transform(model,trainLoader,slice=0,verbose=True): # it actually p
             #    I_c.append(gradientCosin(grad_test, grad_train))
 
             # log the label, the prediction, the confidence, and the influential image
-
-            label_prob = pred.squeeze().detach().numpy()[lab] # what dimension is this?
+            pred_np = pred.squeeze().detach().numpy()
+            label_prob = pred_np[lab] # what dimension is this?
             #logger.info(str(label_prob))
-            inf_id = np.argsort(I_c)[slice]
-            inf_label = int(train_labels[inf_id])
-            inf_sample = train_samples[inf_id].numpy()
+            inf_ids = np.argsort(I_c)[slice]
+            if not isinstance(inf_ids, Iterable):
+                #print(f"inf_ids is {inf_ids}, type is {type(inf_ids)}")
+                inf_ids = [inf_ids]
+
+            inf_labels = [int(train_labels[inf_id]) for inf_id in inf_ids]
+            inf_samples = [arrayToBase64IM(train_samples[inf_id].numpy()) for inf_id in inf_ids]
             sample_dict = {"true_label":int(lab),
+                           "pred_label":int(np.argmax(pred_np)),
                            "sample":arrayToBase64IM(samp),
                            "confidence":str(label_prob),
-                           "closest_label":inf_label,
-                           "closest_sample":arrayToBase64IM(inf_sample)}
+                           "closest_label":inf_labels,
+                           "closest_sample":inf_samples}
 
             #for r in sample_dict.keys():
             #    logger.info(r+ " " + str(type(sample_dict[r])))
@@ -108,7 +118,7 @@ def influence_transform(model,trainLoader,slice=0,verbose=True): # it actually p
             verbose and logger.info(json.dumps(sample_dict))
             #logger.info("Some INFO!!")
 
-            closest_points.append(inf_label)
+            closest_points.append(inf_labels)
 
         return torch.IntTensor(closest_points)
 
--- misc/logging.py
+++ misc/logging.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import io
 import base64
 from PIL import Image
+import json
+from json.decoder import JSONDecodeError
+
+from typing import List
 
 
 
@@ -13,3 +17,56 @@ def arrayToBase64IM(samp):
     image_encoded = base64.b64encode(handle.read())
 
     return image_encoded.decode('utf-8')
+
+def get_entries(lines: List[str]):
+
+    entries = []
+    for line in lines:
+
+        try:
+            line_dict = json.loads(line[(line.find(' - ') + 3):])
+            entries.append(line_dict)
+        except JSONDecodeError:
+            pass
+
+    print(f"got {len(entries)} json entries")
+    return entries
+
+def renderLogAsHTML(logfile):
+    #load it, parse out lines with matching json
+    #return the html
+
+    IF_PATTERN = {"true_label",
+                  "pred_label",
+                   "sample",
+                   "confidence",
+                   "closest_label",
+                   "closest_sample"}
+
+    with open(logfile) as f:
+        dict_entries = get_entries(f.readlines())
+
+    influence_entries = [d for d in dict_entries if IF_PATTERN.issubset(d.keys())]
+
+    html_return = ""
+
+    for entry in influence_entries:
+        html_string = f"""
+        <p>Test sample with label {entry["true_label"]}, preducted as {entry["pred_label"]}.
+        Confidence in true label is {entry["confidence"]}</p>
+        <div><img width=100 src="data:image/png;charset=utf-8;base64, {entry["sample"]}" /></div>
+        <hr>
+        """
+
+        for id, (cl, cs) in enumerate(zip(entry["closest_label"],entry["closest_sample"])):
+
+            closest_entry = f"""
+            <p>Corresponding label Number {id} is {cl}. Corresponding sample is:</p>
+            <div><img width=100 src="data:image/png;charset=utf-8;base64, {cs}" /></div>
+            """
+            html_string += closest_entry
+
+        html_return += html_string + "<hr><br>"
+
+
+    return html_return
